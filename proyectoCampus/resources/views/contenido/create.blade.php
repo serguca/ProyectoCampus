400fc69f08148f8bc35f0c23a9c1fde448203cc3
JoseManuelRomanNavarro <josemanuelroman02@digitechfp.com>
2024-11-07 19:27:58 +0100

    CSS RESUELTO

--- proyectoCampus/resources/views/contenido/create.blade.php
+++ proyectoCampus/resources/views/contenido/create.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{ route('contenido.index') }}">Atras</a>
+<div class="Back">
+<a href="{{ route('contenido.index') }}">Back</a>
+</div>
+<div class="Form">
 <form method='POST' action={{ route('contenido.store')}}>
     @csrf
     <label>Titulo:</label>
@@ -21,4 +24,68 @@
     <input type="submit" value="Create"/>
 
 </form>
+</div>
+<style>
+
+    .Back{
+        width: 50%;
+        margin: 0 auto;
+        text-align: end;
+        padding: 20px;
+        background: linear-gradient(to right, #1f3992, #ffffffc3);
+    }
+
+    .Back a{
+        text-decoration: none;
+        color: #000000;
+        padding: 10px;
+        border-radius: 5px;
+        background: #921f1f00;
+    }
+
+    .Form{
+        overflow-wrap: break-word;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        background: linear-gradient(to right, #ffffffd4, #fffffff5);
+        padding: 20px;
+        width: 50%;
+        margin: 0 auto;
+    
+    }
+
+    form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 10px;
+       
+    }
+
+    input{
+        padding: 10px;
+        margin-bottom: 10px;
+        width: 40%;
+    }
+
+    input[type='submit']{
+        width: 20%;
+        background: #15349221;
+        color: black;
+        border: 1px solid rgb(0, 0, 255);
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    input[type='submit']:hover{
+        transition: 0.3s;
+        background: #153492;
+        color: white;
+    }
+</style>
 @endsection
--- proyectoCampus/resources/views/contenido/show.blade.php
+++ proyectoCampus/resources/views/contenido/show.blade.php
@@ -9,7 +9,7 @@
     
     <div class="Contenido-contenido">
         <h1>Nombre contenido: <br>{{$contenido -> titulo}}</h1>
-        <h2>Descripción</h2>
+        <h2>Descripción:</h2>
         <p>{{$contenido -> descripcion}} <br> </p>
     </div>
 
@@ -19,34 +19,45 @@
     .body{
         width: 80%;
         margin: 0 auto;
+        padding: 20px
     }
 
     .contenido-back{
-       
+        text-align: end;
+        padding: 20px;
+        background: linear-gradient(to right, #1f3992, #ffffffc3);
     }
 
     .back{
-       
+        text-decoration: none;
+        color: #000000;
+        padding: 10px;
+        border-radius: 5px;
+        background: #921f1f00;
     }
 
     .Contenido-contenido{
         overflow-wrap: break-word;
-        background: linear-gradient(to right, #ffffff, #ffffff);
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        background: linear-gradient(to right, #ffffffd4, #fffffff5);
         padding: 20px;
     }
+    
 
     h1{
-        color: #183fb37a;
-        text-align: center
+        font-weight: bold;
+        text-align: center;
+        font-size: 30px;
+        
     }
 
     h2{
         color: #721c24;
-        text-align: center
+        text-align: start
     }
 
     p{
-       
+       text-align: start
       
     }
 
